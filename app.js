a0ccdaa85ae5491ac80dd8bc4163da155e7be9fd
CharlieNOXi <quinteroaguilarc@uvm.edu.ve>
2021-10-25 02:04:58 -0400

    Raiz

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let dotenv = require('dotenv')
+let cors = require('cors')
+let { dbConnect }  = require('./config/mongoconect')
+dotenv.config()
+
+let app = express()
+
+//Configuración del Puerto
+app.set("port", process.env.PORT || 5000)
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+//Routes 
+app.use('/almacen', require('./app/routes'))
+
+dbConnect()
+//Escuchando el servidor en el puerto + la Ruta directa + prefijo: almacen
+app.listen(app.get("port"), () =>
+  console.log(`Servidor corriendo en http://127.0.0.1:${app.get("port")}/almacen`))
